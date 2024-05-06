53f49b43eec8bc90abd2a3ac6b4795a203cf5ff1
Anthony Lima <105724891+anthonylimax@users.noreply.github.com>
2024-05-05 22:49:13 -0300

    Update UserController.java

--- src/main/java/com/example/EcoChargerStation/Controller/UserController.java
+++ src/main/java/com/example/EcoChargerStation/Controller/UserController.java
@@ -1,11 +1,6 @@
 package com.example.EcoChargerStation.Controller;
 
-<<<<<<< HEAD
 import com.example.EcoChargerStation.Dtos.LoginUserDTO;
-=======
-import com.example.EcoChargerStation.Dtos.CreateNewClientDTO;
-import com.example.EcoChargerStation.Models.Supplier;
->>>>>>> main
 import com.example.EcoChargerStation.Models.User;
 import com.example.EcoChargerStation.Repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +16,6 @@ public class UserController {
     private IUserRepository repository;
 
 
-<<<<<<< HEAD
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody LoginUserDTO login){
         try{
@@ -36,7 +30,6 @@ public class UserController {
         catch(Exception e){
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-=======
     @PostMapping("/createclient")
     public ResponseEntity<User> createClient(@RequestBody CreateNewClientDTO login){
         try{
@@ -48,7 +41,6 @@ public class UserController {
         catch(Exception e){
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
->>>>>>> main
         }
     }
 
